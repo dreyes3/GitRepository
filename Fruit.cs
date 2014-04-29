55919c211314e66bd19aa8c3743f86cb53f6399f
David Reyes <dreyes3@carlsonwagonlit.com>
2014-04-29 17:03:31 +0800

    Added new project Fruit to repo

---
+++ Fruit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fruit
+{
+    public class Fruit
+    {
+        public string Color { get; set; }
+        protected string Taste { get; set; }
+        protected string Weight { get; set; }
+
+
+        public Fruit
+        {
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mango mango = new Mango();
+            mango.Color = "yellow";
+            
+        }
+    }
+}
